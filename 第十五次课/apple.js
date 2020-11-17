2a4f2a931908f22cc7b31f724a2cafaa9d62d8b0
QiLiao1 <1322686021@qq.com>
2020-11-17 10:19:14 +0800

    hiahiahia

---
+++ 第十五次课/apple.js
@@ -0,0 +1,9 @@
+function apple() {
+
+    var array = [12, 67, 89, 100, 134, 167, 34, 99];
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        console.log(element);
+    }
+}
+apple();
